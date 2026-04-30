7c88e9622f3d4ec7daf33526decee5a07536959b
Tones12 <tony.travis12@gmail.com>
2026-04-30 22:37:28 +0000

    set up project and started the sql schemas

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jeopardy-backend
-Personal Project 1 on Boot.Dev.
+Personal Project 1 on Boot.Dev
+Creating a Jeopardy clone with a focus on backend functionality and less on the frontend bells and whistles.
---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE clues (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category_id INTEGER,
+    question_text TEXT NOT NULL,
+    answer_text TEXT NOT NULL,
+    dollar_value INTEGER NOT NULL,
+    is_daily_double INTEGER DEFAULT 0,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE players (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    player_name TEXT NOT NULL
+);
+
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_time TEXT NOT NULL,
+    current_round TEXT DEFAULT "Jeopardy"
+);
+
+CREATE TABLE game_clue_state (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER,
+    clue_id INTEGER,
+    is_revealed INTEGER DEFAULT 0,
+    answered_by_player_id INTEGER,
+    FOREIGN KEY (game_id) REFERENCES games(id),
+    FOREIGN KEY (clue_id) REFERENCES clues(id),
+    FOREIGN KEY (answered_by_player_id) REFERENCES players(id)
+);
